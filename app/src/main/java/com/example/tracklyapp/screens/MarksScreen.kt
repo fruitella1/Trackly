9ca84bfdc516dc32305d36a4cbe9404a05940bec
danil <fruitella.man@gmail.com>
2026-08-20 15:33:31 -0700

    remake tasks screen and marks screen

--- app/src/main/java/com/example/tracklyapp/screens/MarksScreen.kt
+++ app/src/main/java/com/example/tracklyapp/screens/MarksScreen.kt
@@ -1,5 +1,7 @@
 package com.example.tracklyapp.screens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,12 +16,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.tracklyapp.data.entity.Activity
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.TopAppBar
+import androidx.compose.ui.graphics.Color
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -58,19 +63,39 @@ fun MarksScreen(
 
 @Composable
 fun MarkItem(activity: Activity) {
-    Row(
+    androidx.compose.material3.Card(
+        shape = RoundedCornerShape(12.dp),
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp)
+            .padding(12.dp)
+            .fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = Color(0xFF1e1e1e)
+        ),
+        border = BorderStroke(2.dp, Color.LightGray)
     ) {
-        Text(activity.date)
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                Text("Duration: ${activity.duration} min")
 
-        Spacer(modifier = Modifier.width(4.dp))
+                Spacer(modifier = Modifier.width(32.dp))
 
-        Text("duration: ${activity.duration} min")
-
-        Spacer(modifier = Modifier.width(4.dp))
-
-        Text(activity.note)
+                Text(activity.date)
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                Text(activity.note, color = Color.Gray)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/tracklyapp/screens/TasksScreen.kt
+++ app/src/main/java/com/example/tracklyapp/screens/TasksScreen.kt
@@ -1,5 +1,6 @@
 package com.example.tracklyapp.screens
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -17,22 +18,27 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.tracklyapp.data.entity.Card
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.tracklyapp.ui.theme.Purple80
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TasksScreen(
     onDeleteClick: (Card) -> Unit,
@@ -42,7 +48,7 @@ fun TasksScreen(
     onHistoryClick: (Card) -> Unit
 ) {
     Scaffold(
-        floatingActionButton = {
+        topBar = { TopAppBar(title = { Text("My activities", fontSize = 28.sp) }) }, floatingActionButton = {
             FloatingActionButton(onAddClick) {
                 Icon(
                     imageVector = Icons.Default.Add,
@@ -77,45 +83,65 @@ fun CardItem(
     onHistoryClick: (Card) -> Unit
 ) {
     var showSheet by remember { mutableStateOf(false) }
+
+    androidx.compose.material3.Card(
+        shape = RoundedCornerShape(12.dp),
+        modifier = Modifier
+            .padding(12.dp)
+            .fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = Color(0xFF1e1e1e)
+        ),
+        border = BorderStroke(2.dp, Color.LightGray)
+    ) {
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { showSheet = true }
+                .padding(12.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        )
+        {
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Column(
+                    modifier = Modifier.weight(1f), verticalArrangement = Arrangement.Center
+                ) {
+                    Text(text = card.name, fontSize = 24.sp, color = Color.White)
+
+                    Text(
+                        text = "Target:${card.timeScore} min.",
+                        fontSize = 18.sp,
+                        color = Color.Gray
+                    )
+                }
+            }
+        }
+    }
     if (showSheet) {
         ModalBottomSheet(onDismissRequest = { showSheet = false }) {
             Column(
                 verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(16.dp)
             ) {
-                Button(onClick = { onDeleteClick(card) }) {
+                Button(onClick = { onDeleteClick(card) }, modifier = Modifier.fillMaxWidth()) {
                     Text("Delete")
                 }
 
                 Spacer(modifier = Modifier.height(8.dp))
 
-                Button(onClick = { onHistoryClick(card) }) {
+                Button(onClick = { onHistoryClick(card) }, modifier = Modifier.fillMaxWidth()) {
                     Text("History")
                 }
 
                 Spacer(modifier = Modifier.height(8.dp))
 
-                Button(onClick = { onMarkClick(card) }) {
+                Button(onClick = { onMarkClick(card) }, modifier = Modifier.fillMaxWidth()) {
                     Text("+Mark")
                 }
-            }
-        }
-    }
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable { showSheet = true },
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    )
-    {
-        Row(modifier = Modifier.fillMaxWidth()) {
-            Column(
-                modifier = Modifier.weight(1f), verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(text = card.name, fontSize = 32.sp)
-                Text(text = "Цель:${card.timeScore} минут.", fontSize = 32.sp)
+                Spacer(modifier = Modifier.height(16.dp))
             }
         }
     }
